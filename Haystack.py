18486e2af8ffa755448cbc3c422232d38284b207
Zuzen <zhuzhan51@gmail.com>
2025-02-08 19:51:06 +0800

    测试Haystack

--- Haystack.py
+++ Haystack.py
@@ -6,13 +6,17 @@ from openai import OpenAI
 os.environ["OPENAI_API_KEY"] = "ollama"
 base_url = 'http://localhost:11434/v1/'
 
-# 下载文件
-import urllib.request
-urllib.request.urlretrieve("https://archive.org/stream/leonardodavinci00brocrich/leonardodavinci00brocrich_djvu.txt", "davinci.txt")
+# 使用本地文件路径
+local_file_path = "davinci.txt"
+
+# #测试文件是否能正常读取
+# with open(local_file_path,"r",encoding="utf-8") as f:
+#     text = f.read()
+#     print(text)
 
 # 创建索引管道
 indexing_pipeline = Pipeline.from_template(PredefinedPipeline.INDEXING)
-indexing_pipeline.run(data={"sources": ["davinci.txt"]})
+indexing_pipeline.run(data={"sources": [local_file_path]})
 
 # 创建RAG管道
 rag_pipeline = Pipeline.from_template(PredefinedPipeline.RAG)
